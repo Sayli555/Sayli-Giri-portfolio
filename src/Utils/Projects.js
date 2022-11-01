8cecf67b3863b4fc618690b79dfa0fb430044788
Sayli Giri <sayligiri555@gmail.com>
2022-11-01 14:25:01 +0530

    Eshop clone add

---
+++ src/Utils/Projects.js
@@ -0,0 +1,74 @@
+export const projects = [
+  {
+
+
+    img: "https://raw.githubusercontent.com/Sayli555/project-images/master/E-shop.png",
+    title: "E-Shop Clone",
+    des: "This is the clone of E-Shop website.which is an e-commerce website of a Cloathings,accessories.This is individual project at Full Stack-development bootcamp in Masai School.",
+    live: "https://e-shop-ruby.vercel.app/",
+    gitHub: "https://github.com/Sayli555/e-shop",
+    technologies: ["React", "Redux", "HTML","Javascript","CSS"],
+  },
+  {
+
+
+    img: "https://user-images.githubusercontent.com/97591028/180641416-56175994-d677-4ca7-9a16-f4063fa30ed0.png",
+    title: "HubSpot Clone",
+    des: "This is the clone of HubSpot website. HubSpot is a marketing , sales and customer service platform that helps companies grow better.. This is our team collaboration project at Full Stack-development bootcamp in Masai School.",
+    demo: "",
+    live: "https://leafy-moxie-ee6b4e.netlify.app/",
+    gitHub: "https://github.com/optimizershivam/Hubspot-clone",
+    technologies: ["React", "Redux", "Chakra UI","Javascript","CSS"],
+  },
+  {
+    img: "https://raw.githubusercontent.com/Sayli555/project-images/master/Screenshot%20(32).png",
+    title: "Beauty-Bebo Clone",
+    des: "This is the clone of Beauty-Bebo website. A web application to buy skincare and makeup products. This is solo project at Full Stack-development bootcamp in Masai School.",
+    demo: "",
+    live: "https://beutybebo-clone.vercel.app/",
+    gitHub: "https://github.com/Sayli555/beutybebo-clone",
+    technologies: ["React", "CSS", "JavaScript","NodeJS","ExpressJS","MongoDB"],
+  },
+  {
+    img: "https://github.com/sahilshekh/project-photos/blob/master/photos/jio-mart.png?raw=true",
+    title: "JioMart Clone",
+    des: "This is the clone of JioMart website. A web application to buy Glocery and Daily-Need products. This is our team collaboration project at Full Stack-development bootcamp in Masai School.",
+    demo: "",
+    live: "https://jio-mart-clone.vercel.app/",
+    gitHub: "https://github.com/jibi966/JioMart-clone",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    img: "https://github.com/sahilshekh/project-photos/blob/master/photos/west-side.png?raw=true",
+    title: "Westside-ecommerce-Clone",
+    des: "This is the clone of Westside Shopping website , which is an e-commerce website of a Cloathings. This is our team collaboration project at Full Stack-development bootcamp in Masai School.",
+    demo: "",
+    live: "https://westside-ecommerce-clone.vercel.app/",
+    gitHub: "https://github.com/mdalishanali/Westside-ecommerce-Clone",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+     
+    ],
+  },
+  {
+    img: "https://github.com/sahilshekh/project-photos/blob/master/photos/purplle.png?raw=true",
+    title: "Purple-Clone",
+    des: "This is the clone of purple-skincare & makeup website. A web application to buy skincare and makeup products. This is solo project at Full Stack-development bootcamp in Masai School.",
+    demo: "",
+    live: "https://inspiring-aryabhata-5a6301.netlify.app/",
+    gitHub: "https://github.com/Sayli555/purple-clone",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    img: "https://miro.medium.com/max/1400/0*_a0kLpcnLpnrWlHB.png",
+    title: "Credo Clone",
+    des: "This is the clone of Credo website. A web application to buy skincare and makeup products. This is our team collaboration project at Full Stack-development bootcamp in Masai School.",
+    demo: "",
+    live: "https://westside-ecommerce-clone.vercel.app/",
+    gitHub: "https://github.com/Sayli555/credo-clone/",
+    technologies: ["HTML", "CSS", "JavaScript","NodeJS","ExpressJS","MongoDB"],
+  },
+ 
+];
