0d5c00ade4e4fa63ce91b4e621953d15f1a7b518
sayali giri <sayligiri555@gmail.com>
2024-12-18 20:36:56 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -17,54 +17,55 @@ function App() {
 	}, []);
 
 	return (
-		<React.Fragment>
-			{!state ? (
-				<div
-					style={{
-						background: `${newTheme.background}`,
-					}}
-					className='logoStart'
-				>
-					<img
-						src='https://github.com/RahulGoyal03/Portfolio/blob/main/src/assets/logorahul.png?raw=true'
-						alt='logo'
-					/>
-				</div>
-			) : (
-				<div className='components'>
-					<div
-						style={{
-							background: `${newTheme.menuBackground}`,
-							color: `${newTheme.title}`,
-							left: `${open ? '-100vw' : '0'}`,
-						}}
-						className='links'
-					>
-						<a onClick={handleMenu} href='#home'>
-							Home
-						</a>
-						<a onClick={handleMenu} href='#about'>
-							About
-						</a>
-						{/* <a onClick={handleMenu} href='#experience'>
-							Experience
-						</a> */}
+		// <React.Fragment>
+		// 	{!state ? (
+		// 		<div
+		// 			style={{
+		// 				background: `${newTheme.background}`,
+		// 			}}
+		// 			className='logoStart'
+		// 		>
+		// 			<img
+		// 				src='https://github.com/RahulGoyal03/Portfolio/blob/main/src/assets/logorahul.png?raw=true'
+		// 				alt='logo'
+		// 			/>
+		// 		</div>
+		// 	) : (
+		// 		<div className='components'>
+		// 			<div
+		// 				style={{
+		// 					background: `${newTheme.menuBackground}`,
+		// 					color: `${newTheme.title}`,
+		// 					left: `${open ? '-100vw' : '0'}`,
+		// 				}}
+		// 				className='links'
+		// 			>
+		// 				<a onClick={handleMenu} href='#home'>
+		// 					Home
+		// 				</a>
+		// 				<a onClick={handleMenu} href='#about'>
+		// 					About
+		// 				</a>
+		// 				{/* <a onClick={handleMenu} href='#experience'>
+		// 					Experience
+		// 				</a> */}
 
-						<a onClick={handleMenu} href='#projects'>
-							Projects
-						</a>
-						<a onClick={handleMenu} href='#techStacks'>
-							Profeciencies
-						</a>
-						<a onClick={handleMenu} href='#contact'>
-							Contact
-						</a>
-					</div>
-					<Navbar />
-					<Home scrollRef={scrollRef} />
-				</div>
-			)}
-		</React.Fragment>
+		// 				<a onClick={handleMenu} href='#projects'>
+		// 					Projects
+		// 				</a>
+		// 				<a onClick={handleMenu} href='#techStacks'>
+		// 					Profeciencies
+		// 				</a>
+		// 				<a onClick={handleMenu} href='#contact'>
+		// 					Contact
+		// 				</a>
+		// 			</div>
+		// 			<Navbar />
+		// 			<Home scrollRef={scrollRef} />
+		// 		</div>
+		// 	)}
+		// </React.Fragment>
+		<></>
 	);
 }
 
